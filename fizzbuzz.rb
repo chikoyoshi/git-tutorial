ee506529e582640dc0283af3591c5af3abfc0b62
j1414 <j1414@matsue-ct.jp>
2017-11-21 09:50:53 +0900

    completed

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-100.times |i|
-	if (i%3 == 0 && i%5 == 0) then 
-		p "FizzBuzz"
-	else if (i%3==0) then
-		p "Fizz"
-	else if (i%5==0) then
-		p "Buzz"
+100.times { |i| 
+	if (i%3 == 0 && i%5 == 0) 
+		print("FizzBuzz\n")
+	elsif (i%3==0) 
+		print("Fizz\n")
+	elsif (i%5==0) 
+		print("Buzz\n")
 	else
 		p i
 	end
-end
+}
