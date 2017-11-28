1681da3bfb8d26daec500feeb53a97a7c7b9edc3
j1414 <j1414@matsue-ct.jp>
2017-11-28 09:15:07 +0900

    Add feature-A

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 100.times { |i| 
-	if (i%3 == 0 && i%5 == 0) 
+	if (i%3 == 0 && i%5 == 0)
 		print("FizzBuzz\n")
-	elsif (i%3==0) 
+	elsif (i%3==0)
 		print("Fizz\n")
-	elsif (i%5==0) 
+	elsif (i%5==0)
 		print("Buzz\n")
 	else
 		p i
